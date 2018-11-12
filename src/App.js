b89b8f7d0e05e04402e89d827e9bce84eb2d7384
Yago Gonzalez <jagogc@gmail.com>
2018-11-12 22:41:46 +0000

    Update App

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Theme from "./providers/Theme";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Theme>
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>Test</p>
+    </header>
+  </Theme>
+);
 
 export default App;
