2700181c5eb0a64a07e8134ff3f5b7268325ff45
Yago Gonzalez <jagogc@gmail.com>
2018-11-12 23:06:41 +0000

    Update App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import styled from "styled-components";
 import Theme from "./providers/Theme";
+
+const Container = styled.div`
+  min-height: 100vh;
+  background: ${props => props.theme.color.david};
+  background-color: #2bedd5;
+  background-image: url("data:image/svg+xml,%3Csvg width='64' height='64' viewBox='0 0 64 64' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M8 16c4.418 0 8-3.582 8-8s-3.582-8-8-8-8 3.582-8 8 3.582 8 8 8zm0-2c3.314 0 6-2.686 6-6s-2.686-6-6-6-6 2.686-6 6 2.686 6 6 6zm33.414-6l5.95-5.95L45.95.636 40 6.586 34.05.636 32.636 2.05 38.586 8l-5.95 5.95 1.414 1.414L40 9.414l5.95 5.95 1.414-1.414L41.414 8zM40 48c4.418 0 8-3.582 8-8s-3.582-8-8-8-8 3.582-8 8 3.582 8 8 8zm0-2c3.314 0 6-2.686 6-6s-2.686-6-6-6-6 2.686-6 6 2.686 6 6 6zM9.414 40l5.95-5.95-1.414-1.414L8 38.586l-5.95-5.95L.636 34.05 6.586 40l-5.95 5.95 1.414 1.414L8 41.414l5.95 5.95 1.414-1.414L9.414 40z' fill='%23ffffff' fill-opacity='0.4' fill-rule='evenodd'/%3E%3C/svg%3E");
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+  text-shadow: 0px 0px 6px rgba(0, 0, 0, 0.4);
+`;
 
 const App = () => (
   <Theme>
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>Test</p>
-    </header>
+    <Container>
+      <p>David</p>
+    </Container>
   </Theme>
 );
 
