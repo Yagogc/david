2dd5a744eed1605dc2afe2492f312e4475ef800e
Yago Gonzalez <jagogc@gmail.com>
2018-11-12 23:06:25 +0000

    Update Theme

--- src/providers/Theme.js
+++ src/providers/Theme.js
@@ -5,7 +5,15 @@ import theme from "../styles/theme";
 
 const GlobalStyle = createGlobalStyle`
   * {
-    font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New";
+    margin: 0;
+    padding: 0;
+  }
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+      "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
   }
 `;
 
